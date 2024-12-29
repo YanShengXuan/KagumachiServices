4affa1c769e688e17c4879905078e8bce72b4739
YanShengXuan <shengxuan.yan1995@gmail.com>
2024-12-29 22:12:31 +0800

    初始化還原點

---
+++ src/main/java/tw/com/services/kagumachi/KagumachiServicesApplication.java
@@ -0,0 +1,13 @@
+package tw.com.services.kagumachi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KagumachiServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KagumachiServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/com/services/kagumachi/KagumachiServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.services.kagumachi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KagumachiServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
