72560da1b3fe634a261cacb89eb92651f2c79170
whj86 <whj906732@gmail.com>
2025-01-13 09:20:59 +0800

    Add ShipRateController getAllShipRates(), updateShipRate()
    Rename WebsiteController -> SettingController

--- src/main/java/tw/com/services/kagumachi/repository/ShipRateRepository.java
+++ src/main/java/tw/com/services/kagumachi/repository/ShipRateRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tw.com.services.kagumachi.model.ShipRate;
 
+import java.util.Optional;
+
 @Repository
 public interface ShipRateRepository extends JpaRepository<ShipRate, Integer> {
+    Optional<ShipRate> findByRegion(String region);
 }
