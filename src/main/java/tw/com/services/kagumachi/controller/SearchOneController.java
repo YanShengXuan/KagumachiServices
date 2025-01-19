a1d7692c3f5ae9d71c344ec3e09277c02becf61d
AgentP324 <l20293982@gmail.com>
2025-01-20 00:14:29 +0800

    0120更新

--- src/main/java/tw/com/services/kagumachi/controller/SearchOneController.java
+++ src/main/java/tw/com/services/kagumachi/controller/SearchOneController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import tw.com.services.kagumachi.model.Product;
@@ -58,5 +59,40 @@ public class SearchOneController {
 			jsonarray.put(jsonobject);	
 			}
 		return jsonarray.toString();
+	}
+	
+	@GetMapping("/sone/{se}/{pri}")
+	public String getnewProducts(@PathVariable String se,@PathVariable Integer pri) {
+		List<Product> list = productrepository.findByProductnameContaining(se);
+//		System.out.println(list.size());
+		int count=0;
+		for (Product product : list) {
+			if(product.getDiscountprice()< pri) {
+				System.out.println(product.getProductname());
+				System.out.println(product.getDiscountprice());
+				count++;
+			}
+		}
+		JSONArray jsonarray = new JSONArray();
+		
+		for(Product product : list) {
+			if(product.getDiscountprice()<= pri) {
+				JSONObject jsonobject = new JSONObject();
+				jsonobject.put("count", count);
+				jsonobject.put("dataname", product.getProductname());
+				
+				int productid = product.getProductid();
+				List<ProductColor> productcolors = productColorRepository.findByProduct_Productid(productid);   
+	            int colorsid = productcolors.get(0).getColorsid();
+	            Optional<ProductImage> productImage = productImageRepository.findByProduct_ProductidAndProductColor_Colorsid(productid, colorsid);//與color資料表連接
+	            String imgurl = productImage.get().getImageurl();
+				jsonobject.put("dataimage", imgurl);
+				jsonobject.put("dataprice", product.getDiscountprice());
+				
+				jsonarray.put(jsonobject);	
+				}
+			}
+		return jsonarray.toString();
+
 	}
 }
--- src/main/java/tw/com/services/kagumachi/repository/OrderDetailRepository.java
+++ src/main/java/tw/com/services/kagumachi/repository/OrderDetailRepository.java
@@ -1,8 +1,12 @@
 package tw.com.services.kagumachi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import tw.com.services.kagumachi.model.OrderDetail;
 @Repository
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, Integer> {}
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Integer> {
+
+}
