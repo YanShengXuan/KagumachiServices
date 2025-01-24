a4dfa30575e3920e37cf6dd78d9eabc79a9b30f7
emmalu0309 <emmalu0309@icloud.com>
2025-01-25 00:25:00 +0800

    新增查詢單獨商品

--- src/main/java/tw/com/services/kagumachi/repository/CartRepository.java
+++ src/main/java/tw/com/services/kagumachi/repository/CartRepository.java
@@ -6,13 +6,17 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import tw.com.services.kagumachi.model.Cart;
+
+//import tw.com.services.kagumachi.model.Cart;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart,Integer> {
 	List<Cart> findByMember_Memberid(@Param("memberId") Integer memberId);
     Optional<Cart> findByMember_MemberidAndProduct_ProductidAndProductColor_Colorsid(Integer memberid, Integer productid, Integer colorsid);
-    
+
     List<Cart> findByMember_MemberidAndIspurchase(Integer memberid, Boolean ispurchase);
+
+
+    int countByMember_Memberid(Integer memberid);
 }
